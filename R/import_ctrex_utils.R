e8416644b1975a73d84c2b8a44389eed2261c46f
Fabian Scheidt <f-scheidt@gmx.de>
2024-08-26 11:45:39 +0200

    Added Cpp function.

---
+++ R/import_ctrex_utils.R
@@ -0,0 +1,7 @@
+#' @details
+#' ctrex is a package showing how to produce packages with
+#' Rcpp Modules and C++ classes and make them available under S4.
+#'
+#' @useDynLib CTrex, .registration = TRUE
+#' @import methods Rcpp
+"_PACKAGE"
