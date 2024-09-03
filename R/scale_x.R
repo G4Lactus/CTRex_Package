0d8388991cd560314cdc5ffb662ce416aeb2b9c6
Fabian Scheidt <f-scheidt@gmx.de>
2024-09-03 15:50:17 +0200

    bug fix.

--- R/scale_x.R
+++ R/scale_x.R
@@ -5,14 +5,29 @@ scale_x <- function(x,
   # center values
   x <- scale(x, center = TRUE, scale = FALSE)
 
+  if (num_rows < 2) {
+    denum <- num_rows
+  } else {
+    denum <- num_rows - 1
+  }
+  sqrt_n <- sqrt(num_rows)
+
   # determine column standard deviations
   x_cstds <- apply(x, 2, function(xs) {
-    return(sqrt(sum(Mod(xs) ** 2)  / (num_rows - 1)))
+    return(sqrt(sum(Mod(xs) ** 2)  / denum))
   })
 
   # normalize
+  tol <- .Machine$double.eps
   for (colX in seq_len(num_cols)) {
-    x[, colX] <- x[, colX] / x_cstds[colX]
+    # x[, colX] <- x[, colX] / x_cstds[colX]
+    col_std <- x_cstds[colX]
+    if (col_std / sqrt_n < tol) {
+      x[, colX] <- tol * sqrt_n
+    } else {
+      x[, colX] <- x[, colX] / col_std
+    }
   }
+
   return(x)
 }
