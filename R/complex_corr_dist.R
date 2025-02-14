4ef1eb190a454b5c63a645ddfaac09f5d48b7f15
Fabian Scheidt <f-scheidt@gmx.de>
2025-02-14 12:03:59 +0100

    File for complex correlation matrix based distance matrix.

---
+++ R/complex_corr_dist.R
@@ -0,0 +1,16 @@
+complex_corr_dist <- function(X) {
+  n <- nrow(X)
+  # NOTE: scaling causes trouble with clustering a steering mtx.
+  #X <- scale(X, center = TRUE, scale = FALSE)
+  cov_mat <- Conj(t(X)) %*% X
+  cov_mat <- cov_mat / (n - 1)
+
+  # Compute variances (diagonal elements)
+  vars <- diag(cov_mat)
+
+  # Compute the correlation matrix
+  corr_mat <- cov_mat / sqrt(outer(vars, vars, "*"))
+
+  # Transform correlation matrix into distance matrix
+  return(as.dist(1 - Mod(corr_mat)))
+}
