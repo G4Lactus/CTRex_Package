8a3ec47e0266e470889f44f9b5997f471e9edddb
Fabian Scheidt <f-scheidt@gmx.de>
2025-03-14 16:32:16 +0100

    Performance update.

--- R/update_cmplx_gvs_dummies.R
+++ R/update_cmplx_gvs_dummies.R
@@ -1,32 +1,37 @@
 update_cmplx_gvs_dummies <- function(cenv = environment()) {
 
-  X_p_sub_dummy <- matrix(NA, nrow = cenv$n, ncol = cenv$p)
-
+  n <- cenv$n
   p <- cenv$p
-  for (w in seq(cenv$w_max)) {
-    for (z in seq(cenv$cluster_res$max_clusters)) {
+  w_max <- cenv$num_dummies / p
+  X_p_sub_dummy_blck <- matrix(NA, nrow = n, ncol = p)
 
-      sub_X <- cenv$X[, cenv$cluster_res$clusters$`clusters$Var`[[z]], drop = FALSE]
-      sigma_sub_X <- complex_cov(sub_X)
-      idx <- cumsum(cenv$cluster_res$cluster_sizes)
-      mu <- rep(0, times = cenv$cluster_res$cluster_sizes[[z]])
+  cluster_sizes <- cenv$cluster_res$cluster_sizes
+  cluster_var <- cenv$cluster_res$clusters$`clusters$Var`
+  max_clusters <- cenv$cluster_res$max_clusters
+  idx <- cumsum(cluster_sizes)
+  X_dummy <- cenv$X_dummy
+
+  for (w in seq_len(w_max)) {
+    for (z in seq(max_clusters)) {
+
+      sigma_sub_X <- complex_cov(cenv$X[, cluster_var[[z]], drop = FALSE])
+      mu <- rep(0, cluster_sizes[z])
 
       if (z == 1) {
-        X_p_sub_dummy[, seq(idx[z])] <- cmvnorm::rcmvnorm(
-                                          cenv$n,
+        X_p_sub_dummy_blck[, seq(idx[z])] <- cmvnorm::rcmvnorm(
+                                          n = n,
                                           mean = mu,
                                           sigma = sigma_sub_X)
 
       } else {
-        X_p_sub_dummy[, seq(idx[z - 1] + 1, idx[z])] <- cmvnorm::rcmvnorm(
-                                                          cenv$n,
+        X_p_sub_dummy_blck[, seq(idx[z - 1] + 1, idx[z])] <- cmvnorm::rcmvnorm(
+                                                          n = n,
                                                           mean = mu,
                                                           sigma = sigma_sub_X)
 
       }
     }
-    cenv$X_dummy[, seq(w * p + 1, (w + 1) * p)] <- X_p_sub_dummy
+    X_dummy[, seq.int(w * p + 1, (w + 1) * p)] <- X_p_sub_dummy_blck
   }
-
-
+  cenv$X_dummy <- X_dummy
 }
