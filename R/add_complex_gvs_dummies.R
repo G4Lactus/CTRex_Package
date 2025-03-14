fca6ab74039fa74a3fd34f4e2b8acdaa4d73d08d
Fabian Scheidt <f-scheidt@gmx.de>
2025-03-14 15:28:57 +0100

    Moved IEN part into if block.

--- R/add_complex_gvs_dummies.R
+++ R/add_complex_gvs_dummies.R
@@ -21,19 +21,8 @@ add_complex_gvs_dummies <- function(X,
                                )
 
   # Generate dummy predictors and append them to original X
-  w_max <- num_dummies / p
   X_dummy <- matrix(NA, nrow = n, ncol = p + num_dummies)
-  X_dummy[, seq(p)] <- X
-
-  if (gvs_type == "cIEN") {
-    # Binary cluster identity vectors for T-Rex+GVS+IEN
-    # ----------------------------------------------------
-    IEN_cl_ind <- lapply(cluster_res$clusters$`clusters$Var`, FUN = function(x) as.numeric(sub(".", "", x)))
-    IEN_cl_id_vectors <- t(sapply(seq_along(IEN_cl_ind), FUN = function(x) {
-      cl_id_vec <- rep(FALSE, times = ncol(X))
-      cl_id_vec[IEN_cl_ind[[x]]] <- TRUE
-      cl_id_vec}))
-  }
+  X_dummy[, seq.int(1, p)] <- X
 
   # Update complex gvs dummies
   update_cmplx_gvs_dummies(cenv = environment())
@@ -48,6 +37,15 @@ add_complex_gvs_dummies <- function(X,
     ))
 
   } else if (gvs_type == "cIEN") { # ----------------------
+    # Binary cluster identity vectors for T-Rex+GVS+IEN
+    # ----------------------------------------------------
+    IEN_cl_ind <- lapply(cluster_res$clusters$`clusters$Var`,
+                         FUN = function(x) as.numeric(sub(".", "", x)))
+    IEN_cl_id_vectors <- t(sapply(seq_along(IEN_cl_ind), FUN = function(x) {
+      cl_id_vec <- rep(FALSE, times = ncol(X))
+      cl_id_vec[IEN_cl_ind[[x]]] <- TRUE
+      cl_id_vec}))
+
     return(
       list(X_dummy = X_dummy,
            max_clusters = cluster_res$max_clusters,
