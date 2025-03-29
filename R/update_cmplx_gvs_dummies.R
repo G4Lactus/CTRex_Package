c64a775533b272cf0baba34504b94f9802ec6317
Fabian Scheidt <f-scheidt@gmx.de>
2025-03-29 15:31:22 +0100

    Added documentation.

--- R/update_cmplx_gvs_dummies.R
+++ R/update_cmplx_gvs_dummies.R
@@ -1,3 +1,39 @@
+#' @title
+#' Update Complex GVS Dummy Variables
+#'
+#' @description
+#' Internal function that generates cluster-structured complex dummy variables
+#' for Group-Variable Selection (GVS). Creates dummy blocks mirroring original
+#' predictor covariance structure within identified clusters.
+#'
+#' @param cenv Environment containing: \itemize{
+#'   \item X: Original complex matrix (n x p)
+#'   \item X_dummy: Matrix to augment (n x (p + num_dummies))
+#'   \item cluster_res: Clustering results from \code{\link{complex_hclust}}
+#'   \item num_dummies: Total dummies to generate
+#'   \item n: Number of observations
+#'   \item p: Number of original variables
+#' }
+#' Modifies cenv$X_dummy in-place.
+#'
+#' @keywords internal
+#'
+#' @examples
+#' \donttest{
+#' # Typically called internally by add_complex_gvs_dummies()
+#' X <- matrix(complex(real=rnorm(100), imaginary=rnorm(100)), ncol=20)
+#' env <- new.env()
+#' env$X <- X
+#' env$X_dummy <- matrix(complex(), nrow=10, ncol=40)
+#' env$cluster_res <- complex_hclust(X, "ward.D2", "dynamic")
+#' env$num_dummies <- 20
+#' env$n <- 10
+#' env$p <- 20
+#'
+#' update_cmplx_gvs_dummies(env)
+#' dim(env$X_dummy) # 10 x 40 complex matrix
+#' }
+#'
 update_cmplx_gvs_dummies <- function(cenv = environment()) {
 
   # -- old
@@ -32,7 +68,6 @@ update_cmplx_gvs_dummies <- function(cenv = environment()) {
   # }
 
 
-
   # new ----
   n <- cenv$n
   p <- cenv$p
@@ -58,7 +93,7 @@ update_cmplx_gvs_dummies <- function(cenv = environment()) {
                                       n = n,
                                       mean = rep(0, cluster_sizes[z]),
                                       sigma = complex_cov(
-                                        cenv$X[, cluster_var[[z]], drop = FALSE]
+                                          cenv$X[, cluster_var[[z]], drop = FALSE] # drop arg must be kept!
                                         )
                                     )
 
